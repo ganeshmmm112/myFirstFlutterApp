6bc0422bcdeb8cadde1289dd5cd2cc9a8561a14c
GaneshKumar <ganeshmmm112@gmail.com>
2022-08-16 13:09:25 +0530

    Initial App Scratch

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+//Dated  : 16.08.2022
+//author : Ganesh Kumar
+//file   : lib/main.dart
+
+import 'package:flutter/material.dart';
+
+// void main(){
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello!'),);
+  }
+}
