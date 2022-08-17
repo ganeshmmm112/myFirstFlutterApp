6b48be289597c3e906c5a43f5ad2bef6834c5930
GaneshKumar <ganeshmmm112@gmail.com>
2022-08-17 12:16:42 +0530

    First Styling & Layouting steps

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-   final String questionText; //This class (or a class that this class inherits from) is marked as '@immutable', but one or more of its instance fields aren't final: Question.questionText.dart
+  final String questionText;
+  //This class (or a class that this class inherits from) is marked as '@immutable', but one or more of its instance fields aren't final: Question.questionText.dart
 
   Question(this.questionText); // constructor (Positional Arguments)
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container( // For Styling Similar to Bootstrap
+      width: double.infinity,// response according to screen size
+      margin:EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center, //enum - human readable
+      ),
+    );
   }
 }
